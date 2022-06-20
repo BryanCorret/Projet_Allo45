d9f342e22e94b71b2d58c44e60d106485183926f
BOURDERE_ANDREOU Nathan <o22100472@info23-24.iut45.univ-orleans.fr>
2022-06-20 11:50:55 +0200

    ajouts des images sur les boutons et ajout de profils de méthodes nécéssaires pour le fonctionnement de l'appli

--- src/appliSondage.java
+++ src/appliSondage.java
@@ -1,5 +1,7 @@
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Scene;
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import javafx.scene.image.ImageView;
@@ -10,9 +12,12 @@ import javafx.scene.control.*;
 import javafx.scene.control.ToggleGroup;
 import java.util.List;
 import java.util.Arrays;
-import javafx.geometry.Pos;
+import java.util.ArrayList;
+import javafx.scene.chart.Chart;
 
 public class appliSondage extends Application{
+
+    private RequetesSQL lesRequetes;
 
     private Button boutonConnexion;
 
@@ -44,16 +49,20 @@ public class appliSondage extends Application{
         this.boutonHome = new Button(); //image home
         this.boutonParam = new Button(); //image Param
         this.boutonRefresh = new Button(); //image f5
-        this.boutonDeconnexion = new Button("Se déconnecter");
+        this.boutonDeconnexion = new Button(); //image deconnexion
         ImageView home = new ImageView("./home.png");
         ImageView refresh = new ImageView("./reload.png");
         ImageView deco = new ImageView("./Disconnect.png");
+        ImageView param = new ImageView("./parametre.jpg");
         home.setFitHeight(30);home.setFitWidth(30);
         refresh.setFitHeight(30);refresh.setFitWidth(30);
         deco.setFitHeight(30);deco.setFitWidth(30);
+        param.setFitHeight(30);deco.setFitWidth(30);
         this.boutonHome.setGraphic(home);
         this.boutonRefresh.setGraphic(refresh);
         this.boutonDeconnexion.setGraphic(deco);
+        this.boutonParam.setGraphic(param);
+        this.boutonParam.setStyle("-fx-background-color:transparent;");
         this.boutonHome.setStyle("-fx-background-color:transparent;");
         this.boutonConnexion.setStyle("-fx-background-color:transparent;");
         this.boutonDeconnexion.setStyle("-fx-background-color:transparent;");
@@ -95,13 +104,13 @@ public class appliSondage extends Application{
 
 
     public void modeDonneesBrutes(){
-        Pane root = new FenetreDonneesBrutes(this.btnConnexion);
+        Pane root = new FenetreDonneesBrutes(this.btnConnexion); //fenetre pas encore faite
         this.scene.setRoot(root);
         root.getScene().getWindow().sizeToScene();
     }
 
     public void modeSondeur(){
-        Pane root = new FenetreSondeur(this.btnConnexion);
+        Pane root = new FenetreSondeur(this.btnConnexion); //fenetre pas encore faite
         this.scene.setRoot(root);
         root.getScene().getWindow().sizeToScene(); //redimensionne le root à la place nécéssaire à l'affichage de l'appli
     }
@@ -135,6 +144,23 @@ public class appliSondage extends Application{
     public void EnregistrerReponses(){
 
     }
+
+    public void majAffichageSondeur(){
+
+    }
+    
+    public List<String> rechercherSondage(String StrRecherche){
+        List<String> listeDesSondages = new ArrayList<String>();
+        return listeDesSondages;
+    }
+
+    public void quitter(){
+        Platform.exit();
+    }
+
+    // public Chart Diagrammes(){
+        
+    // }
 
     public static void main(String[] args){
         Application.launch(args);
