a7614ce0c8c675933898dd6a06b5441c04b6f18d
Kirizkon <bryan.corret@gmail.com>
2022-06-21 09:19:20 +0200

    manque le modele

--- src/ControleurConnexion.java
+++ src/ControleurConnexion.java
@@ -2,6 +2,7 @@ import javax.print.DocFlavor.STRING;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.control.Alert;
 
 
 public class ControleurConnexion implements EventHandler<ActionEvent> {
@@ -21,7 +22,24 @@ public class ControleurConnexion implements EventHandler<ActionEvent> {
         String Mdp = fenConnexion.getMdp();
         System.out.println("Nom utilisateur : "+ NomU+ "Mdp : "+ Mdp);
 
-        //this.sondage.Connexion(NomU, Mdp); 
+        // this.sondage.Connexion(NomU, Mdp)
+        if(this.sondage.estCorrect(NomU, Mdp)){
+            System.out.println("Connexion réussie");
+            // POp up
+            this.sondage.Connexion(NomU, Mdp);
+            Alert AlertConnexion = new Alert(Alert.AlertType.INFORMATION);
+            AlertConnexion.setTitle("Connexion réussie");
+            AlertConnexion.setHeaderText("Bienvenue "+NomU);
+            AlertConnexion.setContentText("Vous êtes connecté");
+        }
+        else{
+            System.out.println("Connexion échouée");
+            Alert AlertErreur = new Alert(Alert.AlertType.INFORMATION);
+            AlertErreur.setTitle("Connexion échouée");
+            AlertErreur.setHeaderText("Erreur");
+            AlertErreur.setContentText("Votre nom d'utilisateur ou votre mot de passe est incorrect");
+            
+        } 
 
     }
 }
